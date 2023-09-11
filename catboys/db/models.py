d75940a59dbb6e9261436ec36aad8088c8aef57a
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2023-09-10 22:28:58 -0400

    remove file extension

--- catboys/db/models.py
+++ catboys/db/models.py
@@ -8,13 +8,9 @@ class Media(Base):
     __tablename__ = "media"
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True)
-    file_extension: Mapped[str]
 
     url: Mapped[str] = ""
     artist_url: Mapped[str] = ""
     artist_name: Mapped[str] = ""
     source_url: Mapped[str] = ""
     anime: Mapped[str] = ""
-
-    def get_file_path(self):
-        return pathlib.Path("media") / self.id / self.file_extension
